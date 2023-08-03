fdc2809fd5cb26cec480d249139e9590bbdd413e
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-08-03 17:38:31 +0200

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,78 @@
+<div class="container">
+    <div class="lists-container">
+      <div class="list">
+        <h3>DC COMICS</h3>
+        <ul>
+          <li>Characters</li>
+          <li>Comics</li>
+          <li>Movies</li>
+          <li>TV</li>
+          <li>Games</li>
+          <li>Videos</li>
+          <li>News</li>
+        </ul>
+        <h3>SHOP</h3>
+        <ul>
+          <li>Shop DC</li>
+          <li>Shop DC Collectibles</li>
+        </ul>
+      </div>
+      <div class="list">
+        <h3>DC</h3>
+        <ul>
+          <li>Terms Of Use</li>
+          <li>Privacy policy (New)</li>
+          <li>Ad Choices</li>
+          <li>Advertising</li>
+          <li>Jobs</li>
+          <li>Subscriptions</li>
+          <li>Talent Workshops</li>
+          <li>CPSC Certificates</li>
+          <li>Ratings</li>
+          <li>Shop Help</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+      <div class="list">
+        <h3>SITES</h3>
+        <ul>
+          <li>DC</li>
+          <li>MAD Magazine</li>
+          <li>DC Kids</li>
+          <li>DC Universe</li>
+          <li>DC Power Visa</li>
+        </ul>
+      </div>
+
+    </div>
+    <div class="img-container">
+      <img src="../assets/img/dc-logo-bg.png" alt="">
+    </div>
+  </div>
+  <div class="shop-cont">
+    <div class="container">
+      <ul>
+        <li>
+          <img src="../assets/img/buy-comics-digital-comics.png" alt=" t-shirt">
+          <a href="#">DIGITAL COMICS</a>
+        </li>
+        <li>
+          <img src="../assets/img/buy-comics-merchandise.png" alt=" t-shirt">
+          <a href="#">DC MERCHANDISE</a>
+        </li>
+        <li>
+          <img src="../assets/img/buy-comics-subscriptions.png" alt=" t-shirt">
+          <a href="#">SUBSCRIPTION</a>
+        </li>
+        <li>
+          <img src="../assets/img/buy-comics-shop-locator.png" alt=" t-shirt">
+          <a href="#">COMIC SHOP LOCATOR</a>
+        </li>
+        <li>
+          <img src="../assets/img/buy-dc-power-visa.svg" alt=" t-shirt">
+          <a href="#">DC POWER VISA</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
