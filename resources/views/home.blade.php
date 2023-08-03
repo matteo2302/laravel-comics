4e9daeb36f31b9f509803918832f1d2c078b7a39
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-08-03 17:58:36 +0200

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,83 +8,12 @@
 </head>
 <body>
   @include('includes.header')
-<div class="container">
-  <div class="card">
-    <img src="https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY">
-    <h4>Action Comics</h4>
-  </div>
-  <div class="card">
-    <img src="https://imgs.search.brave.com/OU2E3_3vA7flyv-SJ3TcFW1d99KDlp5ZfmW9ylLLFDo/rs:fit:1012:1200:1/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vd3d3LnN1/cGVycG91dm9pci5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MjAvMDcvQW1lcmlj/YW4tVmFtcGlyZS0x/OTc2LTAxLWNvdi1B/bGJ1cXVlcnF1ZS0y/MDIwLmpwZz9yZXNp/emU9MTAxMiUyQzE1/NTYmc3NsPTE">
-    <h4>American Vampire 1976</h4>
-  </div>
-  <div class="card">
-    <img src="https://imgs.search.brave.com/gAZQuZyt6WjcUWF6aXCIBaiJqtOJAK-X4nxS9Vbvwno/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9jZG4y/LnBlbmd1aW4uY29t/LmF1L2NvdmVycy9v/cmlnaW5hbC85Nzgx/NDAxMjc1NDI2Lmpw/Zw">
-    <h4>Aquaman</h4>
-  </div>
-  <div class="card">
-    <img src="https://imgs.search.brave.com/YET9wY29Aa29TyHqlbieS9qsz3HWyNYQx-WkRqdBRX4/rs:fit:640:984:1/g:ce/aHR0cDovL2dldHlv/dXJjb21pY29uLmNv/LnVrL3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDE2LzA4L2JhdGdp/cmwtcmViaXJ0aC0x/LWNvdmVyLmpwZw">
-    <h4>Batgirl</h4>
-  </div>
-  <div class="card">
-    <img src="https://imgs.search.brave.com/jgxYlrx442aozemzjqdmCsd9DkIkRollJp1T8sG8TfE/rs:fit:720:1106:1/g:ce/aHR0cHM6Ly9veXN0/ZXIuaWduaW1ncy5j/b20vd29yZHByZXNz/L3N0Zy5pZ24uY29t/LzIwMTgvMTAvU1RM/MDk3MDk1LTcyMHgx/MTA2LmpwZw">
-    <h4>Batman</h4>
-  </div>
-  <div class="card">
-    <img src="https://imgs.search.brave.com/jgxYlrx442aozemzjqdmCsd9DkIkRollJp1T8sG8TfE/rs:fit:720:1106:1/g:ce/aHR0cHM6Ly9veXN0/ZXIuaWduaW1ncy5j/b20vd29yZHByZXNz/L3N0Zy5pZ24uY29t/LzIwMTgvMTAvU1RM/MDk3MDk1LTcyMHgx/MTA2LmpwZw">
-    <h4>Batman Beyond</h4>
-  </div>
-  <div class="card">
-    <img src="https://imgs.search.brave.com/cSaB3m-tghN3_Cllp9IOlUPEPsyShCnl8gijRyPFdrg/rs:fit:791:1200:1/g:ce/aHR0cHM6Ly8xNDk0/NTUxNTIudjIucHJl/c3NhYmxlY2RuLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAx/OS8wOC9CTVNNX0N2/MV9CQVQuanBn">
-    <h4>Batman/Superman</h4>
-  </div>
-  <div class="card">
-    <img src="https://imgs.search.brave.com/kSLCPBIeE9s218LRC4cIyrHOYmxrSS2Wt7LvNNfED2I/rs:fit:455:700:1/g:ce/aHR0cHM6Ly9jb21p/Y2Jvb2tyZWFsbS5j/b20vY292ZXItc2Nh/bi81ZmFmMTFjYTk1/NGQ1MGVjZDA1ZmU3/M2U1NjBiNWQyOC94/bC8tLWlzc3VlLTEu/anBn">
-    <h4>Batman/Superman Annual</h4>
-  </div>
-  <div class="card">
-    <img src="https://imgs.search.brave.com/a6fAPUra4t94xMJFlYv6X7Lwm4z_qPmslx7Hiqafbok/rs:fit:600:923:1/g:ce/aHR0cHM6Ly9jZG4u/ZmxpY2tlcmluZ215/dGguY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDIwLzA4L0Jh/dG1hbi1UaHJlZS1K/b2tlcnMtMS02MDB4/OTIzLTEuanBn">
-    <h4>Batman: The Joker War Zone</h4>
-  </div>
-  <div class="card">
-    <img src="https://imgs.search.brave.com/qMxyCwK99aguaETp4ioxXLrpxwNg2JNBN3Oq7LGAgzM/rs:fit:780:1200:1/g:ce/aHR0cHM6Ly9jZG4u/ZmxpY2tlcmluZ215/dGguY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDIwLzEwL0Jh/dG1hbi1XaGl0ZS1L/bmlnaHQtUHJlc2Vu/dHMtSGFybGV5LVF1/aW5uLTEtMS5qcGc">
-    <h4>Batman: Three Jokers</h4>
-  </div>
-  <div class="card">
-    <img src="https://imgs.search.brave.com/bf_FlGnymAgmvweY8YiLAcyy600JdSMl_jHpdR3t5ro/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvNTA5/M2IxOTEtZTkzZS00/NTJjLThhYTQtYTEw/ZDY0ZTcxN2E5LjA5/N2UwZWM5NDgzYzky/OGExMjY4Nzk1Nzk2/NTliZWNjLmpwZWc_/b2RuV2lkdGg9NjEy/Jm9kbkhlaWdodD02/MTImb2RuQmc9ZmZm/ZmZm">
-    <h4>Batman: White Knight Presents: Harley Quinn</h4>
-  </div>
-  <div class="card">
-    <img src="https://imgs.search.brave.com/bf_FlGnymAgmvweY8YiLAcyy600JdSMl_jHpdR3t5ro/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvNTA5/M2IxOTEtZTkzZS00/NTJjLThhYTQtYTEw/ZDY0ZTcxN2E5LjA5/N2UwZWM5NDgzYzky/OGExMjY4Nzk1Nzk2/NTliZWNjLmpwZWc_/b2RuV2lkdGg9NjEy/Jm9kbkhlaWdodD02/MTImb2RuQmc9ZmZm/ZmZm">
-    <h4>Catwoman</h4>
-  </div>
-</div>
-<div class="shop-cont">
-    <div class="container">
-      <ul>
-        <li>
-          <img src="../resources/img/buy-comics-digital-comics.png" alt=" t-shirt">
-          <a href="#">DIGITAL COMICS</a>
-        </li>
-        <li>
-          <img src="../resources/img/buy-comics-merchandise.png" alt=" t-shirt">
-          <a href="#">DC MERCHANDISE</a>
-        </li>
-        <li>
-          <img src="../resources/img/buy-comics-subscriptions.png" alt=" t-shirt">
-          <a href="#">SUBSCRIPTION</a>
-        </li>
-        <li>
-          <img src="../resources/img/buy-comics-shop-locator.png" alt=" t-shirt">
-          <a href="#">COMIC SHOP LOCATOR</a>
-        </li>
-        <li>
-          <img src="../resources/img/buy-dc-power-visa.svg" alt=" t-shirt">
-          <a href="#">DC POWER VISA</a>
-        </li>
+  <div class="cards-container">
+ @include('includes.cards')
+ <button>Load More</button>
+ </div>
+ @include('includes.shop')
 
-      </ul>
-    </div>
-  </div>
   @include('includes.footer')
 </body>
 </html>
---
+++ resources/views/includes/shop.blade.php
@@ -0,0 +1,27 @@
+<div class="shop-cont">
+    <div class="container">
+      <ul>
+        <li>
+          <img src="../resources/img/buy-comics-digital-comics.png" alt=" t-shirt">
+          <a href="#">DIGITAL COMICS</a>
+        </li>
+        <li>
+          <img src="../resources/img/buy-comics-merchandise.png" alt=" t-shirt">
+          <a href="#">DC MERCHANDISE</a>
+        </li>
+        <li>
+          <img src="../resources/img/buy-comics-subscriptions.png" alt=" t-shirt">
+          <a href="#">SUBSCRIPTION</a>
+        </li>
+        <li>
+          <img src="../resources/img/buy-comics-shop-locator.png" alt=" t-shirt">
+          <a href="#">COMIC SHOP LOCATOR</a>
+        </li>
+        <li>
+          <img src="../resources/img/buy-dc-power-visa.svg" alt=" t-shirt">
+          <a href="#">DC POWER VISA</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
