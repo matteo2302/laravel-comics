290f6893e76117d036e3a23ea74f2f4edd9045c4
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-08-03 18:09:14 +0200

    footer

---
+++ resources/views/includes/socials.blade.php
@@ -0,0 +1,15 @@
+<div class="socials-footer">
+    <div class="container">
+      <button><a href="#">SIGN-UP NOW</a></button>
+      <div class="socials-list">
+        <h2>FOLLOW US</h2>
+        <ul>
+          <li><a href="#"><img src="../assets/img/footer-facebook.png" alt="Facebook"></a></li>
+          <li><a href="#"><img src="../assets/img/footer-twitter.png" alt="Twitter"></a></li>
+          <li><a href="#"><img src="../assets/img/footer-youtube.png" alt="Youtube"></a></li>
+          <li><a href="#"><img src="../assets/img/footer-pinterest.png" alt="Pinterest"></a></li>
+          <li><a href="#"><img src="../assets/img/footer-periscope.png" alt="Preiscope"></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
