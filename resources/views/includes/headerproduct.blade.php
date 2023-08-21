321c2b0e886485fea12870f3f7b326410aa53286
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-08-21 17:47:48 +0200

    product basics

---
+++ resources/views/includes/headerproduct.blade.php
@@ -0,0 +1,42 @@
+<div class="container">
+    <nav>
+      <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
+      <ul>
+        <li><a href="#"></a>Characters</li>
+        <li><a href="#"></a>Comics</li>
+        <li><a href="#"></a>Movies</li>
+        <li><a href="#"></a>TV</li>
+        <li><a href="#"></a>Games</li>
+        <li><a href="#"></a>Collectibles</li>
+        <li><a href="#"></a>Videos</li>
+        <li><a href="#"></a>Fans</li>
+        <li><a href="#"></a>News</li>
+        <li><a href="#"></a>Shop</li>
+      </ul>
+    </nav>
+  </div>
+  <div class="main-bg">
+    
+    <img src="{{ $product['thumb']}}"><div>
+    <h3>{{$product['title']}}</h3>
+    <div class="price"></div>
+    <p>{{$product['description']}}</p></div>
+    <div>
+      <p>advertisement</p>
+      <img src="{{ Vite::asset('resources/img/adv.jpg') }}" >
+    </div>
+  </div>
+  <div class="container">
+    <div>
+    <thead><th></th></thead>
+  <tbody>
+    <td></td>
+    <td></td>
+  </tbody></div>
+  <div>
+    <thead><th></th></thead>
+  <tbody>
+    <td></td>
+    <td></td>
+  </tbody></div>
+  </div>
---
+++ resources/views/product.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/js/app.js');
+  <title>DC comics</title>
+</head>
+<body>
+  @include('includes.header')
+  <div class="cards-container">
+</div>
+ @include('includes.shop')
+
+  @include('includes.footer')
+</body>
+</html>
